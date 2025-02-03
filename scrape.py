9d8e84017925ed9f56e794a65d24cc7b15686b28
dominique Taplin <Dtaplin21@aol.com>
2025-02-02 16:53:35 -0800

    add selenum

--- scrape.py
+++ scrape.py
@@ -6,20 +6,25 @@ def scrape_website(website):
     print("Launching Chrome Browser...")
 
 
-    chrome_driver_path = "./chromedriver"         #allows to control chrome
-    options = webdriver.ChromeOptions()   #how the chrome will oporate
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options) #setup driver
-
-
-    try:
-        driver.get(website)
-        print("Page loaded...")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        print('Connected! Navigating...')
+        driver.get('https://example.com')
+        print('Taking page screenshot to file page.png')
+        driver.get_screenshot_as_file('./page.png')
+        print('Navigated! Scraping page content...')
         html = driver.page_source
-        time.sleep(10)
+        print(html)
 
-        return html
-    finally:
-        driver.quit()
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
+AUTH = 'brd-customer-hl_97f6e034-zone-ai_scraper:76q03ch4g3jf'
+SBR_WEBDRIVER = f'https://{AUTH}@brd.superproxy.io:9515'
+
+
+if __name__ == '__main__':
+  main()
 
 # click buttons and interact with the page 
 # control web broweser
